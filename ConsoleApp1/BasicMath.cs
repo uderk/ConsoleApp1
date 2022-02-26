c91de7b4fa24252a5a8160ede2c65b7acb9e8ed5
uderk <venislav.zdravkov@gmail.com>
2022-02-26 18:32:15 +0200

    this is initial commit for the project

---
+++ ConsoleApp1/BasicMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class BasicMath
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Substract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            double res = 0.0;
+            try
+            {
+                res = num1 / num2;
+                return res;
+            }
+            catch(DivideByZeroException exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            return res;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num2 * num1;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Test the Add Method
+        [TestMethod]
+        public void TestMethod_Add()
+        {
+            BasicMath bm1 = new BasicMath();
+            double res = bm1.Add(10, 15);
+            Assert.AreEqual(res, 25);
+        }
+
+        // Test the SubstractMethod
+        [TestMethod]
+        public void TestMethod_Substract()
+        {
+            BasicMath bm2 = new BasicMath();
+            double res = bm2.Substract(30, 15);
+            Assert.AreEqual(res, 15);
+        }
+
+        //Test the Divide Method
+        [TestMethod]
+        public void TestMethod_Divide()
+        {
+            BasicMath bm = new BasicMath();
+            double res = bm.Divide(20, 5);
+            Assert.AreEqual(res, 4);
+        }
+
+        //Testing the Multilplay method
+        [TestMethod]
+        public void TestMethod_Multiply()
+        {
+            BasicMath bm = new BasicMath();
+            double res = bm.Multiply(10, 15);
+            Assert.AreEqual(res, 150);
+        }
+
+
+    }
+}
